569f1acf5fdc96508dbb7ecda609287a5e73397e
Sachin Sasikumar <sachinsasikumarclt@gmail.com>
2026-01-17 16:56:41 +0530

    Applied Force, Imposed BC, solved for u

--- main.cpp
+++ main.cpp
@@ -4,6 +4,48 @@
 #include <iomanip> //For std::setw (Make the output look like a grid)
 
 
+// --- TOOL: GAUSSIAN ELIMINATION SOLVER ---
+// Solves [K]{u} = {F} for {u}
+// You don't need to memorize this. It's standard algebra.
+
+std::vector<double> solve_system(std::vector<std::vector<double>> K, std::vector<double> F){
+    int n = F.size();
+
+    //1. Forward elimination
+
+    for (int i = 0; i<n; i ++){
+        double pivot = K[i][i];
+        for (int j = i; j<n; j++)K[i][j]/= pivot;
+        F[i] /= pivot;
+
+        //Eliminate rows below
+
+        for (int k = i+1; k<n; k++){
+            double factor = K[k][i];
+            for (int j = i; j<n; j++) K[k][j] -= factor*K[i][j];
+            F[k] -= factor * F[i];
+        }
+
+        }
+
+        //2. Backward Substitution
+
+        std::vector<double> u(n);
+
+        for (int i = n-1; i >=0; i--){
+            double sum = 0;
+            for (int k = i+1; k<n; k++) sum += K[i][k]*u[k];
+            u[i] = F[i] - sum;
+        }
+
+        return u;
+            
+        }
+
+
+//--- MAIN FEM CODE --- 
+
+
 int main() {
     std::cout << "\n--- FEM SOLVER V1.2 GLOBAL ASSEMBLY---" << std::endl;
 
@@ -57,16 +99,37 @@ int main() {
     }
 
 
-    //---4. PRINT THE RESULTS ---
-    std::cout<<"\n--- FINAL GLOBAL STIFFNESS MATRIX ---"<<std::endl;
+    //---4. APPLY LOADS (Force Vector)---
+    // We want to pull node 3 to the right by 100 Newtons
+    std::vector<double> F(num_nodes, 0);
+    F[2] = 100;
+    std::cout<<"Applied F = 100 N at Node 3"<<std::endl;
 
-    for(int i=0; i < num_nodes; i++){
-        std::cout<<"[";
-        for (int j = 0; j<num_nodes; j++){
-            std::cout<<std::setw(6)<<k_global[i][j]<<" ";
-        }
-        std::cout<<"]"<<std::endl;
+    //---5. Apply Boundary Conditions (Fix node 1)---
+    // We enforce u1 = 0 using the '1-0-0' method
+    std::cout<<"Fixing Node 1 (u=0)..."<<std::endl;
+    int fixed_node_idx = 0; //Node 1 is at index 0
+
+    //A. Zero out the row
+    for (int j = 0; j< num_nodes; j++){
+        k_global[fixed_node_idx][j] = 0;
     }
+
+    //B. Set diagonal to 1 and Force to 0
+    k_global[fixed_node_idx][fixed_node_idx] = 1;
+    F[fixed_node_idx] = 0;
+
+    //---6. Solve k_global{u} = {F}---
+    std::cout<<"Solving system..."<<std::endl;
+    std::vector<double> displacements = solve_system(k_global, F);
+
+    //---7. PRINT THE RESULTS---
+    std::cout<<"\n---RESULTS---"<<std::endl;
+    std::cout<<"Node \t Displacement(u)"<<std::endl;
+    for (int i = 0; i<num_nodes; i++){
+        std::cout<<i+1<<" \t"<<displacements[i]<<" m"<<std::endl;
+    }
+
 
     return 0;
 
